07072642880e9391fe173839cecc5d706e6f04ed
iadsa <iputuagusadityasaputra@gmail.com>
2023-10-19 21:35:44 +0800

    nambah haha hehe nih

--- index.php
+++ index.php
@@ -1,11 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>VI Food</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>VI Food</title>
 </head>
+
 <body>
-	<h1>Helo</h1>
+    <h1>Helo</h1>
+
+    <ul>
+        <li>hehe</li>
+        <li>hhhh</li>
+        <li>hahaha</li>
+    </ul>
+
 </body>
+
 </html>
