828db8869a50c061bf5da546f4e70a2b5bb71a0f
iadsa <iputuagusadityasaputra@gmail.com>
2023-10-21 08:21:39 +0800

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-<<<<<<< HEAD
-  mode: "jit",
-  content: ["./**/*.{html}"],
-=======
   mode: 'jit',
   content: ["./**/*.html"],
->>>>>>> 342b7beec807fc6003790f5e9e1bdba60dabd501
   theme: {
     extend: {
       colors: {
